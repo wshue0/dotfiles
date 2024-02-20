2b3e15ec5d2026ac99f203b7dd028a05f35fe477
wshue0 <wshue0@github.com>
2024-02-19 22:42:42 -0500

    Configure sptlrx widget

--- scripts/.scripts/widgets.sh
+++ scripts/.scripts/widgets.sh
@@ -9,7 +9,7 @@ alacritty --hold -t hexblock --class hexblock -e ~/.scripts/hex-block.sh &
 alacritty --hold -t star --class star -e starfetch -r &
 alacritty -t clock --class clock -e tty-clock -t -c &
 alacritty --hold -t fetch --class fetch -o font.size=9 -e neofetch --stdout &
-alacritty --hold -t lyrics --class lyrics -o font.size=9 -e sptlrx --current "bold,#F5C2E7" &
+alacritty --hold -t lyrics --class lyrics -o font.size=16 -e sptlrx --current "bold,#F5C2E7" &
 sleep 1
 
 i3-msg '[class = "vis" instance = "vis"] kill'
@@ -26,15 +26,15 @@ alacritty --hold -t hexblock --class hexblock -e ~/.scripts/hex-block.sh &
 alacritty --hold -t star --class star -e starfetch -r &
 alacritty -t clock --class clock -e tty-clock -t -c &
 alacritty --hold -t fetch --class fetch -o font.size=9 -e neofetch --stdout &
-alacritty --hold -t lyrics --class lyrics -o font.size=9 -e sptlrx --current "bold,#F5C2E7" &
+alacritty --hold -t lyrics --class lyrics -o font.size=16 -e sptlrx --current "bold,#F5C2E7" &
 
 
 sleep 1
 
-i3-msg '[class = "vis" instance = "vis"] floating enable, resize set 1200 700, move position 2303 1140'
+i3-msg '[class = "vis" instance = "vis"] floating enable, resize set 1200 700, move position 2303 790'
 i3-msg '[class = "fetch" instance = "fetch"] floating enable, resize set 722 391, move position 949 1500'
 i3-msg '[class = "arch" instance = "arch"] floating enable, resize set 567 567, move position 326 1406'
 i3-msg '[class = "hexblock" instance = "hexblock"] floating enable, resize set 1550 184, move position 2180 1861'
 i3-msg '[class = "star" instance = "star"] floating enable, resize set 1270 484, move position 183 631'
 i3-msg '[class = "clock" instance = "clock"] floating enable, resize set 800 400, move position 2500 300'
-i3-msg '[class = "lyrics" instance = "lyrics"] floating enable, resize set 800 400, move position 2500 300'
+i3-msg '[class = "lyrics" instance = "lyrics"] floating enable, resize set 1000 300, move position 2403 1525'
