6b62f4371a378d1ac7e90dbef969c0c215aac8c5
wshue0 <wshue0@github.com>
2024-11-24 14:04:06 -0500

    update widgets script

--- scripts/.scripts/widgets.sh
+++ scripts/.scripts/widgets.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
 # Load clock and stuff
-i3-msg "workspace --no-auto-back-and-forth 10"
-
 alacritty -t vis --class vis -e vis &
 alacritty --hold -t arch --class arch -o font.size=9 -e neofetch --logo &
 alacritty --hold -t hexblock --class hexblock -e ~/.scripts/hex-block.sh &
@@ -10,25 +8,6 @@ alacritty --hold -t star --class star -e starfetch -r &
 alacritty -t clock --class clock -e tty-clock -t -c &
 alacritty --hold -t fetch --class fetch -o font.size=9 -e neofetch --stdout &
 alacritty --hold -t lyrics --class lyrics -o font.size=16 -e sptlrx --current "bold,#F5C2E7" &
-sleep 1
-
-i3-msg '[class = "vis" instance = "vis"] kill'
-i3-msg '[class = "fetch" instance = "fetch"] kill'
-i3-msg '[class = "arch" instance = "arch"] kill'
-i3-msg '[class = "hexblock" instance = "hexblock"] kill'
-i3-msg '[class = "star" instance = "star"] kill'
-i3-msg '[class = "clock" instance = "clock"] kill'
-i3-msg '[class = "lyrics" instance = "lyrics"] kill'
-
-alacritty -t vis --class vis -e vis &
-alacritty --hold -t arch --class arch -o font.size=9 -e neofetch --logo &
-alacritty --hold -t hexblock --class hexblock -e ~/.scripts/hex-block.sh &
-alacritty --hold -t star --class star -e starfetch -r &
-alacritty -t clock --class clock -e tty-clock -t -c &
-alacritty --hold -t fetch --class fetch -o font.size=9 -e neofetch --stdout &
-alacritty --hold -t lyrics --class lyrics -o font.size=16 -e sptlrx --current "bold,#F5C2E7" &
-
-
 sleep 1
 
 i3-msg '[class = "vis" instance = "vis"] floating enable, resize set 1200 700, move position 2303 790'
@@ -38,3 +17,15 @@ i3-msg '[class = "hexblock" instance = "hexblock"] floating enable, resize set 1
 i3-msg '[class = "star" instance = "star"] floating enable, resize set 1270 484, move position 183 631'
 i3-msg '[class = "clock" instance = "clock"] floating enable, resize set 800 400, move position 2500 300'
 i3-msg '[class = "lyrics" instance = "lyrics"] floating enable, resize set 1000 300, move position 2403 1525'
+
+i3-msg '[class = "vis" instance = "vis"] move to workspace 10'
+i3-msg '[class = "fetch" instance = "fetch"] move to workspace 10'
+i3-msg '[class = "arch" instance = "arch"] move to workspace 10'
+i3-msg '[class = "hexblock" instance = "hexblock"] move to workspace 10'
+i3-msg '[class = "star" instance = "star"] move to workspace 10'
+i3-msg '[class = "clock" instance = "clock"] move to workspace 10'
+i3-msg '[class = "lyrics" instance = "lyrics"] move to workspace 10'
+
+sleep 1
+
+i3-msg "workspace --no-auto-back-and-forth 10"
